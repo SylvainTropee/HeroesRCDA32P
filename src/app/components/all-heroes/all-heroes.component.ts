e9f9b279f12e8b9efdb59b589e44f48387c1da06
stropee <stropee@campus-eni.fr>
2024-06-07 11:27:39 +0200

    ajout image + interface

--- src/app/components/all-heroes/all-heroes.component.ts
+++ src/app/components/all-heroes/all-heroes.component.ts
@@ -1,26 +1,37 @@
 import {Component} from '@angular/core';
-import {JsonPipe} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
+import {Hero} from "../../models/hero";
 
 @Component({
   selector: 'app-all-heroes',
   standalone: true,
   imports: [
-    JsonPipe
+    JsonPipe,
+    NgForOf
   ],
   templateUrl: './all-heroes.component.html',
   styleUrl: './all-heroes.component.css'
 })
 export class AllHeroesComponent {
 
-  public heroes: any[] //Array<any>
+  public heroes: Hero[] //Array<any>
 
   constructor() {
     this.heroes = []
   }
 
   addHeroes() {
-    this.heroes.push({name: "Batman"})
-    this.heroes.push({name: "Wonder Woman"})
-    this.heroes.push({name: "Hulk"})
+    this.heroes.push({
+      name: "Batman",
+      image: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg"
+    })
+    this.heroes.push({
+      name: "Wonder Woman",
+      image: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/720-wonder-woman.jpg"
+    })
+    this.heroes.push({
+      name: "Hulk",
+      image: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/332-hulk.jpg"
+    })
   }
 }
